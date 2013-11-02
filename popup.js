375a57cb31a97e2c615baa5d349e1d0ed784a4fc
Jony Cheung <jonathan.cheung@am.sony.com>
2013-11-01 22:12:07 -0700

    Adding functionality to enable "failsafe" using the browserAction button

--- popup.js
+++ popup.js
@@ -1,5 +1,32 @@
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  chrome.tabs.sendMessage(tabs[0].id, {greeting: "start"}, function(response) {
-    
-  });
-});
+var tablink = "";
+
+function click(e) {
+  var newLink = updateQueryStringParameter(tablink, "failsafe", "true");
+  chrome.tabs.executeScript(null,
+      {code:"window.location.href='"+newLink+"'"});
+  window.close()
+}
+
+$(document).on("ready", function(e){
+   chrome.tabs.getSelected(null,function(tab) {
+    tablink = tab.url;
+    if (tablink.indexOf("failsafe=true") == -1)
+        $("#failsafe").on("click", click)
+    else
+        $("#failsafe").addClass("disabled");
+  });   
+})
+
+function updateQueryStringParameter(uri, key, value) {
+  var re = new RegExp("([?|&])" + key + "=.*?(&|$)", "i");
+  separator = uri.indexOf('?') !== -1 ? "&" : "?";
+  if (uri.match(re)) {
+    return uri.replace(re, '$1' + key + "=" + value + '$2');
+  }
+  else {
+    if (uri.indexOf("#")>-1)
+        return [uri.slice(0, uri.indexOf("#")), separator + key + "=" + value, uri.slice(uri.indexOf("#"))].join('');
+    else
+        return uri + separator + key + "=" + value;
+  }
+}
