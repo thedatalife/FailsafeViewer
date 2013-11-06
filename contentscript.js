83b6bd093a6f727f8983c2c047d6a9033806805d
Jony Cheung <jonathan.cheung@am.sony.com>
2013-11-06 10:44:05 +0200

    minor refactoring some code.

--- contentscript.js
+++ contentscript.js
@@ -10,6 +10,9 @@ window.onload = function(){
     $(".visuallyhidden pre").each(function() {
       new FailSafe($(this));
     });
+    if (document.location.href.indexOf("showImgInfo=true") > -1){
+        iQInfo.showIqInfo();
+    }
     // showIqInfo();
     console.log("FailsafeViewer Chrome Extension initialized");
 }
@@ -17,30 +20,37 @@ window.onload = function(){
 // requires: 
 //      jQuery
 // adds the 
-var showIqInfoEnabled = false;
 
-function showIqInfo(){
-    if (showIqInfoEnabled) return;
-    showIqInfoEnabled = true;
-    $(".iq-img").each(function(index,elm){
-        var infoString = '';
-        infoString += "desktop: <a href=\""+$(elm).data("src-desktop")+"\" target=\"_blank\">"+ getURLParameter($(elm).data("src-desktop"), "wid") + "</a>, ";
-        infoString += "desktop-2X: <a href=\""+$(elm).data("src-desktop-highres")+"\" target=\"_blank\">"+ getURLParameter($(elm).data("src-desktop-highres"), "wid")+ "</a><br>";
-        infoString += "tablet: <a href=\""+$(elm).data("src-tablet")+"\" target=\"_blank\">"+ getURLParameter($(elm).data("src-tablet"), "wid") + "</a>, ";
-        infoString += "tablet-2X: <a href=\""+$(elm).data("src-tablet-highres")+"\" target=\"_blank\">"+ getURLParameter($(elm).data("src-tablet-highres"), "wid")+ "</a><br>";
-        infoString += "phone: <a href=\""+$(elm).data("src-phone")+"\" target=\"_blank\">"+ getURLParameter($(elm).data("src-phone"), "wid") + "</a>, ";
-        infoString += "phone-2X: <a href=\""+$(elm).data("src-phone-highres")+"\" target=\"_blank\">"+ getURLParameter($(elm).data("src-phone-highres"), "wid")+ "</a><br>";
-        var newElement = iQInfoElement(infoString);
-        $(elm).after(newElement);
-        $(newElement).draggable();
-    })
-}
-
-function getURLParameter(string, name) {
-    return decodeURI(
-        (RegExp(name + '=' + '(.+?)(&|$)').exec(string)||[,null])[1]
-    );
-}
-function iQInfoElement(info_string){
-    return $('<div class="iq-info-div">'+info_string+'</div>').get(0);
-}
+var iQInfo = (function(){
+    var showIqInfoEnabled  = false
+    function iQInfoElement(info_string){
+        return $('<div class="iq-info-div">'+info_string+'</div>').get(0);
+    }
+    function showIqInfo(){
+        if (showIqInfoEnabled) return;
+        showIqInfoEnabled = true;
+        $(".iq-img").each(function(index,elm){
+            var infoString = '';
+            infoString += "desktop: <a href=\""+$(elm).data("src-desktop")+"\" target=\"_blank\">"+ getURLParameter($(elm).data("src-desktop"), "wid") + "px</a>, ";
+            infoString += "desktop-2X: <a href=\""+$(elm).data("src-desktop-highres")+"\" target=\"_blank\">"+ getURLParameter($(elm).data("src-desktop-highres"), "wid")+ "px</a><br>";
+            infoString += "tablet: <a href=\""+$(elm).data("src-tablet")+"\" target=\"_blank\">"+ getURLParameter($(elm).data("src-tablet"), "wid") + "px</a>, ";
+            infoString += "tablet-2X: <a href=\""+$(elm).data("src-tablet-highres")+"\" target=\"_blank\">"+ getURLParameter($(elm).data("src-tablet-highres"), "wid")+ "px</a><br>";
+            infoString += "phone: <a href=\""+$(elm).data("src-phone")+"\" target=\"_blank\">"+ getURLParameter($(elm).data("src-phone"), "wid") + "px</a>, ";
+            infoString += "phone-2X: <a href=\""+$(elm).data("src-phone-highres")+"\" target=\"_blank\">"+ getURLParameter($(elm).data("src-phone-highres"), "wid")+ "px</a>";
+            var newElement = iQInfoElement(infoString);
+            $(elm).after(newElement);
+            $(newElement).draggable();
+        })
+    }
+    function getURLParameter(string, name) {
+        return decodeURI(
+            (RegExp(name + '=' + '(.+?)(&|$)').exec(string)||[,null])[1]
+        );
+    }
+    return api = {
+        showIqInfoEnabled: {
+            get: function(){return showIqInfoEnabled}
+        },
+        showIqInfo: function(){showIqInfo()}
+    }
+})()
--- popup.js
+++ popup.js
@@ -1,57 +1,87 @@
-var tablink = "";
+var URLManipulator = (function(){
+    //Class return
+    function URLManipulator(url_link){
+        this.url = url_link;
+        this.updateParam = function(key, value){
+            return this.url  = updateQueryStringParameter(this.url, key, value);
+        },
+        this.removeParamString = function(param_string){
+            return this.url = this.url.replace("?"+param_string,"") || this.url.replace("&"+param_string,"");;
+        }
+        return this;
+    }
+    // Uility functions
+    function updateQueryStringParameter(uri, key, value) {
+      var re = new RegExp("([?|&])" + key + "=.*?(&|$)", "i");
+      separator = uri.indexOf('?') !== -1 ? "&" : "?";
+      if (uri.match(re)) {
+        return uri.replace(re, '$1' + key + "=" + value + '$2');
+      }
+      else {
+        if (uri.indexOf("#")>-1)
+            return [uri.slice(0, uri.indexOf("#")), separator + key + "=" + value, uri.slice(uri.indexOf("#"))].join('');
+        else
+            return uri + separator + key + "=" + value;
+      }
+    }
+    // URLManipulator API
+    return api =  {
+        create: function(url_link){
+            return new URLManipulator(url_link);
+        }
+    }
+})();
 
-function enableClick(e) {
-  var newLink = updateQueryStringParameter(tablink, "failsafe", "true");
+
+
+var urlObject;
+
+function refreshPage(url){
   chrome.tabs.executeScript(null,
-      {code:"window.location.href='"+newLink+"'"});
+  {code:"window.location.href='"+url+"'"});
+}
+function enableClick(e) {
+  urlObject.updateParam("failsafe", "true");
+  refreshPage(urlObject.url);
   window.close()
 }
 
 function disableClick(e) {
-  var newLink = tablink.replace("?failsafe=true","") || tablink.replace("&failsafe=true","")
-  chrome.tabs.executeScript(null,
-      {code:"window.location.href='"+newLink+"'"});
+  urlObject.updateParam("failsafe", "false");
+  refreshPage(urlObject.url);
   window.close()
 }
 
 function enableImageWidths(e){
-  chrome.tabs.executeScript(null,
-      {code:"showIqInfo()"});
-  $("#seeImageWidths").off("click", enableImageWidths);
+    urlObject.updateParam("showImgInfo", "true");
+  refreshPage(urlObject.url);
+  window.close()
+}
+
+function disableImageWidths(e){
+  urlObject.updateParam("showImgInfo", "false");
+  refreshPage(urlObject.url);
   window.close()
 }
 
 $(document).on("ready", function(e){
    chrome.tabs.getSelected(null,function(tab) {
-    tablink = tab.url;
-    if (tablink.indexOf("failsafe=true") == -1)
+    urlObject = URLManipulator.create(tab.url); 
+    if (urlObject.url.indexOf("failsafe=true") == -1){
         $("#failsafe").on("click", enableClick)
-    else{
+        $("#seeImageWidths").addClass("hidden");
+    }else{
         $("#failsafe").addClass("disabled");
-        $("#failsafe").on("click", disableClick)
-    }
-    chrome.tabs.executeScript(null, {code:"showIqInfoEnabled"}, function(results){
-        if (results && results[0] && results[0] == true){
-            $("#seeImageWidths").addClass("disabled");
+        $("#failsafe").on("click", disableClick);
+        
+        if (urlObject.url.indexOf("showImgInfo=true") > -1){
+          $("#seeImageWidths").addClass("disabled");
+          $("#seeImageWidths").on("click", disableImageWidths);
         }else{
-            $("#seeImageWidths").on("click", enableImageWidths);
+          $("#seeImageWidths").on("click", enableImageWidths);
         }
-    });
-
-    
-  });   
+    }
+  });
 })
 
-function updateQueryStringParameter(uri, key, value) {
-  var re = new RegExp("([?|&])" + key + "=.*?(&|$)", "i");
-  separator = uri.indexOf('?') !== -1 ? "&" : "?";
-  if (uri.match(re)) {
-    return uri.replace(re, '$1' + key + "=" + value + '$2');
-  }
-  else {
-    if (uri.indexOf("#")>-1)
-        return [uri.slice(0, uri.indexOf("#")), separator + key + "=" + value, uri.slice(uri.indexOf("#"))].join('');
-    else
-        return uri + separator + key + "=" + value;
-  }
-}
+
